bbf43ef678f68d13ba912da1de60b66cca15981b
OscarChavez6991 <oscar.chavez@uab.edu.bo>
2024-05-12 21:28:06 -0400

    Consultas mongoose

--- rutas/celularRutas.js
+++ rutas/celularRutas.js
@@ -2,7 +2,7 @@ const express = require('express');
 const rutas = express.Router();
 const CelularModel = require('../models/Celular');
 
-//endpoint traer todos los celulares
+//ENDPOINT 1 traer todos los celulares
 rutas.get('/traerCelulares',async(req, res) => {
     try {
         const celular = await CelularModel.find();
@@ -11,7 +11,7 @@ rutas.get('/traerCelulares',async(req, res) => {
         res.status(500).json({mensaje: error.message});
     }
 });
-//endpoint Crear celular
+//ENDPOINT 2 crear celular
 rutas.post('/crearCelular', async (req, res) => {
     const celular = new CelularModel({
         marca: req.body.marca,
@@ -28,7 +28,7 @@ rutas.post('/crearCelular', async (req, res) => {
     }
 });
 
-//endpoint 3. Editar Celular
+//ENDPOINT 3 editar Celular
 rutas.put('/editarCelular/:id', async (req, res) => {
     try {
         const celularEditado = await CelularModel.findByIdAndUpdate(req.params.id, req.body, { new : true });
@@ -42,16 +42,46 @@ rutas.put('/editarCelular/:id', async (req, res) => {
     }
 });
 
-//ENDPOINT 4. Eliminar Celular
+//ENDPOINT 4 eliminar Celular
 rutas.delete('/eliminarCelular/:id',async (req, res) => {
     try {
        const celularEliminado = await CelularModel.findByIdAndDelete(req.params.id);
        if (!celularEliminado)
-            return res.status(404).json({ mensaje : 'Celular no encontrada!!!'});
+            return res.status(404).json({ mensaje : 'Celular no encontrado!!!'});
        else 
             return res.json({mensaje :  'Celular eliminado'});    
        } 
     catch (error) {
+        res.status(500).json({ mensaje :  error.message})
+    }
+});
+
+//ENDPOINT 5 buscar por marca
+rutas.get('/buscarCelularesporMarca/:marca',async(req, res) => {
+    try {
+        const celular = await CelularModel.find({ marca: req.params.marca});
+        res.json(celular);
+    }catch (error){
+        res.status(500).json({mensaje: error.message});
+    }
+});
+
+//ENDPOINT 6 buscar por bateria
+rutas.get('/buscarCelularesporBateria/:bateria',async(req, res) => {
+    try {
+        const celular = await CelularModel.find({ bateria: {$gt:req.params.bateria}});
+        res.json(celular);
+    }catch (error){
+        res.status(500).json({mensaje: error.message});
+    }
+});
+
+//ENDPOINT 7 ordenar por bateria
+rutas.get('/ordenarCelulares', async (req, res) => {
+    try {
+       const celularesOrdenados = await CelularModel.find().sort({ bateria: -1});
+       res.status(200).json(celularesOrdenados);
+    } catch(error) {
         res.status(500).json({ mensaje :  error.message})
     }
 });
