29f92197dd89d6c50cf93c42a47d935fc4ff9aed
OscarChavez6991 <oscar.chavez@uab.edu.bo>
2024-05-14 21:14:34 -0400

    autenticacion

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const authRutas = require('./rutas/authRutas')
+const Usuario = require('./models/Usuario');
 require('dotenv').config();
 const app = express();
 //ruta
@@ -18,5 +21,21 @@ mongoose.connect(MONGO_URI).then(
     }
 ).catch( error => console.log('error de conexion', error));
 
+const autenticar = async (req, res, next)=>{
+    try{
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token)
+            res.status(401).json({mensaje: 'No existe el token'});
+        const decodificar = jwt .verify(token, 'clave_secreta');
+        req.usuario = await Usuario.findById(decodificar.usuarioId);
+        next();
+    }
+    catch(error){
+        res.status(400).json({error: 'Token invalido'});
+    }
+}
+
+app.use('/auth', authRutas);
+app.use('/celular', autenticar, celularRutas);
 //utilizar las rutas de celular
-app.use('/celular',celularRutas)
+//app.use('/celular',celularRutas)
